c260815a84bee2aaae8f91ba3d02c32da6e2fcd0
AboutWhite <kloss.mela@gmail.com>
2021-08-18 18:40:36 +0200

    init

---
+++ code.js
@@ -0,0 +1,40 @@
+console.log(fetch('https://reqres.in/api/users'));
+
+
+//load list of words when load the window
+//init();
+
+
+//show translation and info when click on word
+show = function (i){
+        document.getElementById("word-text").innerHTML = dictionary[i].german;
+        document.getElementById("info").innerHTML = dictionary[i].info;
+}
+
+search = function() {
+        let query = document.getElementById('search').value;
+
+        if (query == ""){
+                return;
+        }
+
+        //init found to false
+        let found = -1;
+
+        for(let i=0; i< dictionary.length; i++){
+                if(query == dictionary[i].english){
+                        found = i;
+                        break;
+                }else {
+                        document.getElementById("word-text").innerHTML = "word not found";
+                        document.getElementById("info").innerHTML ="";
+                }
+        }
+
+        if ( found >= 0){
+                show (found);
+
+        }
+}
+
+       
---
+++ data.js
@@ -0,0 +1,23 @@
+export let dictionary = [
+    {
+        english: "apple",
+        german:"Apfel",
+        info: "a sort of fruit"
+    },
+    {
+        english: "banana",
+        german:"Banane",
+        info: "a sort of fruit"
+    },
+    {
+        english: "cherry",
+        german:"Kirsche",
+        info: "a sort of fruit"
+    },
+    {
+        english: "kiwi",
+        german:"Kiwi",
+        info: "a sort of fruit"
+    }
+
+]
